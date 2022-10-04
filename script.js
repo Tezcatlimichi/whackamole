4ac92b61a1c73330dcdc632ae8eaf21018ba918e
Kathya Castaneda <kathyakathya.kc@gmail.com>
2022-10-04 14:37:16 -0500

    added function to keep score

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let startButton = document.getElementById('startBtn')
 
 let moles = document.querySelectorAll('.mole')
 
+let score = 0
+//let counter = 0
+
 let start = () => {
   moles.forEach((mole) => {
     mole.style.visibility = 'hidden'
@@ -14,3 +17,29 @@ let start = () => {
   setInterval(popOut, 1000)
 }
 startButton.addEventListener('click', start)
+
+moles.forEach((mole) => {
+  let whac = () => {
+    score += 10
+    document.querySelector('h3').innerText = score
+    checkForWinner()
+  }
+  mole.addEventListener('click', whac)
+})
+// let checkForWinner = () => {
+//   if (score === 100) {
+//     document.querySelector('h1')
+//   }
+// }
+// if ((randomMole.style.visibility = '')) {
+//   let popIn = () => {
+//     randomMole.style.visibility = 'hidden'
+//   }
+//   setInterval(popIn, 1000)
+// }
+
+// counter++
+// console.log(counter)
+// if (counter === 3 && randomMole.style.visibility === "") {
+//   randomMole.style.visibility = 'hidden'
+//}
